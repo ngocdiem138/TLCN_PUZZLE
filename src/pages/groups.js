d17bb7f8e272605ce9624387c0d4cff6bc09e477
ngocdiem138 <19110335@student.hcmute.edu.vn>
2022-12-15 13:42:26 +0700

    no message

--- src/pages/groups.js
+++ src/pages/groups.js
@@ -2,14 +2,14 @@ import {Router} from "@reach/router"
 import React from 'react';
 import JobDetails from "./job-details";
 import SearchGrid from "./search-list-2";
-import DashboardJobPost from "./dashboard-jobPost";
+// import DashboardJobPost from "./dashboard-jobPost";
 import 'babel-polyfill';
 
 const Groups = () => (
     <div>
         <Router>
             <JobDetails path="/groups/job-details/:id" />
-            <DashboardJobPost path="/groups/dashboard-jobPost?id='id'" />
+            {/* <DashboardJobPost path="/groups/dashboard-jobPost?id='id'" /> */}
             <JobDetails path="/groups/candidate-profiles-2" />
             <JobDetails path="/groups/search-list-2?title='title'&city='city'"/>
         </Router>
