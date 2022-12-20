e55cb3c827795e28b4e4c693ca9cab143219cb48
ngocdiem138 <19110335@student.hcmute.edu.vn>
2022-12-21 00:51:17 +0700

    log in with gg

--- src/components/ModalSignEmployer/ModalSignEmployer.js
+++ src/components/ModalSignEmployer/ModalSignEmployer.js
@@ -75,7 +75,8 @@ class Login extends Component {
       <ModalStyled
         size="lg"
         centered
-        show={gContext.signInModalVisible}
+        // show={gContext.signInModalVisible}
+        show={false}
         onHide={gContext.toggleSignInModal}
       >
         <Modal.Body className="p-0">
@@ -131,7 +132,7 @@ class Login extends Component {
                       </a>
                     </div>
                     <div className="col-4 col-xs-12">
-                      <a
+                      {/* <a
                         href="/#"
                         className="font-size-4 font-weight-semibold position-relative text-white bg-poppy h-px-48 flex-all-center w-100 px-6 rounded-5 mb-4"
                       >
@@ -139,7 +140,7 @@ class Login extends Component {
                         <span className="d-none d-xs-block">
                           Log in with Google
                         </span>
-                      </a>
+                      </a> */}
                     </div>
                     <div className="col-4 col-xs-12">
                       <a
--- src/components/ModalSignIn/ModalSignIn.js
+++ src/components/ModalSignIn/ModalSignIn.js
@@ -9,6 +9,9 @@ import { formReset } from "../../actions/auth-actions";
 import styled from "styled-components";
 import { Modal } from "react-bootstrap";
 import GlobalContext from '../../context/GlobalContext';
+import GoogleLogin from 'react-google-login';
+import axios from 'axios';
+import { gapi } from 'gapi-script';
 
 const ModalStyled = styled(Modal)`
   /* &.modal {
@@ -35,6 +38,13 @@ class Login extends Component {
 
   componentDidMount() {
     this.props.formReset();
+    const initClient = () => {
+      gapi.client.init({
+        clientId: '79738345802-r10d4rcntc96lkss7k75jp55vnj5pgvb.apps.googleusercontent.com',
+        scope: ''
+      });
+    };
+    gapi.load('client:auth2', initClient);
   }
 
   onClickSignIn = (event) => {
@@ -45,10 +55,12 @@ class Login extends Component {
 
     this.props.login(data, this.props.history);
     const { error, success } = this.props;
-    if(success){
+    if (success) {
       this.handleClose()
     }
   };
+
+
 
   handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -57,15 +69,69 @@ class Login extends Component {
       [name]: value
     });
   };
+  signup(res) {
+
+
+    const googleresponse = {
+      Name: res.name,
+      email: res.email,
+      token: res.googleId,
+      Image: res.imageUrl,
+      ProviderId: 'Google'
+    };
+
+
+
+    debugger;
+
+
+    // axios.post(API_REST_URL, googleresponse)
+
+
+    //   .then((result) => {
+
+
+    //     let responseJson = result;
+
+    //     this.props.history.push('/Dashboard')
+
+
+    //   });
+
+
+  };
+
 
   render() {
     const { email, password, showPass } = this.state;
     const { error, success } = this.props;
     const gContext = this.context;
 
-    if(success){
+    if (success) {
       this.handleClose()
     }
+
+    const responseGoogle = (response) => {
+
+
+      console.log(response);
+
+
+      var res = response.profileObj;
+
+
+      console.log(res);
+
+
+      debugger;
+
+
+      this.signup(response);
+
+
+    }
+
+
 
     return (
       <ModalStyled
@@ -127,16 +193,27 @@ class Login extends Component {
                       </a>
                     </div>
                     <div className="col-4 col-xs-12">
-                      <a
-                        href="/#"
+                      {/* <button
+                        onClick={this.signInWithGoogle}
                         className="font-size-4 font-weight-semibold position-relative text-white bg-poppy h-px-48 flex-all-center w-100 px-6 rounded-5 mb-4"
                       >
                         <i className="fab fa-google pos-xs-abs-cl font-size-7 ml-xs-4"></i>{" "}
                         <span className="d-none d-xs-block">
                           Log in with Google
                         </span>
-                      </a>
+                      </button > */}
+                      <GoogleLogin
+                        className="font-size-4 font-weight-semibold position-relative text-white bg-poppy h-px-48 flex-all-center w-100 px-6 rounded-5 mb-4"
+                        clientId="79738345802-r10d4rcntc96lkss7k75jp55vnj5pgvb.apps.googleusercontent.com"
+                        buttonText="Sign in with Google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                        isSignedIn={true}
+                      />
                     </div>
+
+
                     <div className="col-4 col-xs-12">
                       <a
                         href="/#"
@@ -225,9 +302,9 @@ class Login extends Component {
                     </div>
                     {error ? <div className="alert alert-danger col-12" role="alert">{error}</div> : null}
                     {success ? <div className="alert alert-success col-12" role="alert">{success}</div> : null}
-                      <button className="btn btn-primary btn-medium w-100 rounded-5 text-uppercase">
-                        Log in{" "}
-                      </button>
+                    <button className="btn btn-primary btn-medium w-100 rounded-5 text-uppercase">
+                      Log in{" "}
+                    </button>
                     <p className="font-size-4 text-center heading-default-color">
                       Don’t have an account?{" "}
                       <a href="/#" className="text-primary">
@@ -239,8 +316,8 @@ class Login extends Component {
               </div>
             </div>
           </div>
-        </Modal.Body>
-      </ModalStyled>
+        </Modal.Body >
+      </ModalStyled >
     );
   }
 }
