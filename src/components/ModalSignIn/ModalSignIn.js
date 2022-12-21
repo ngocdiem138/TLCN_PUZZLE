5ac0f5d518b4f5e92971fbf09ada68b94e5be8d8
ngocdiem138 <19110335@student.hcmute.edu.vn>
2022-12-21 23:52:42 +0700

    log in with gg

--- src/components/ModalSignIn/ModalSignIn.js
+++ src/components/ModalSignIn/ModalSignIn.js
@@ -9,8 +9,8 @@ import { formReset } from "../../actions/auth-actions";
 import styled from "styled-components";
 import { Modal } from "react-bootstrap";
 import GlobalContext from '../../context/GlobalContext';
-import { GoogleLogin } from 'react-google-login';
-import { gapi } from 'gapi-script';
+// import { GoogleLogin } from 'react-google-login';
+// import { gapi } from 'gapi-script';
 
 const ModalStyled = styled(Modal)`
   /* &.modal {
@@ -38,13 +38,13 @@ class Login extends Component {
 
   componentDidMount() {
     this.props.formReset();
-    const initClient = () => {
-      gapi.client.init({
-        clientId: '79738345802-r10d4rcntc96lkss7k75jp55vnj5pgvb.apps.googleusercontent.com',
-        scope: ''
-      });
-    };
-    gapi.load('client:auth2', initClient);
+    // const initClient = () => {
+    //   gapi.client.init({
+    //     clientId: '79738345802-r10d4rcntc96lkss7k75jp55vnj5pgvb.apps.googleusercontent.com',
+    //     scope: ''
+    //   });
+    // };
+    // gapi.load('client:auth2', initClient);
   }
 
   onClickSignIn = (event) => {
@@ -67,10 +67,12 @@ class Login extends Component {
       [name]: value
     });
   };
+  responseGoogle = (response) => {
+    var res = response.profileObj;
+    this.signup(response);
+  }
 
   signup(res) {
-
-
     const googleresponse = {
       Name: res.name,
       email: res.email,
@@ -78,53 +80,18 @@ class Login extends Component {
       Image: res.imageUrl,
       ProviderId: 'Google'
     };
-
-
-
-    debugger;
-
-
     // axios.post(API_REST_URL, googleresponse)
-
-
     //   .then((result) => {
-
-
     //     let responseJson = result;
-
     //     this.props.history.push('/Dashboard')
-
-
     //   });
-
-
   };
-
-  
 
   render() {
     const { email, password, showPass } = this.state;
     const { error, success } = this.props;
     const gContext = this.context;
-    const responseGoogle = (response) => {
-
-
-      console.log(response);
-
-
-      var res = response.profileObj;
-
-
-      console.log(res);
-
-
-      debugger;
-
-
-      this.signup(response);
-
-
-    }
+    
 
     if (success) {
       this.handleClose()
@@ -194,8 +161,8 @@ class Login extends Component {
                         className="font-size-4 font-weight-semibold position-relative text-white bg-poppy h-px-48 flex-all-center w-100 px-6 rounded-5 mb-4"
                         clientId="79738345802-r10d4rcntc96lkss7k75jp55vnj5pgvb.apps.googleusercontent.com"
                         buttonText="Sign in with Google"
-                        onSuccess={responseGoogle}
-                        onFailure={responseGoogle}
+                        onSuccess={this.responseGoogle}
+                        onFailure={this.responseGoogle}
                         cookiePolicy={'single_host_origin'}
                         isSignedIn={true}
                       />
