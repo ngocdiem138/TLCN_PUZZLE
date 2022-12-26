a50da2e7c8ee542b9cf77206cb3c4e0a66f52350
ngocdiem138 <19110335@student.hcmute.edu.vn>
2022-12-27 01:36:06 +0700

    no message

--- src/components/ModalSignIn/ModalSignIn.js
+++ src/components/ModalSignIn/ModalSignIn.js
@@ -40,9 +40,9 @@ class Login extends Component {
   componentDidMount() {
     this.props.formReset();
     const gapi =  typeof window === 'object' ? require('gapi-cjs') : () => false
-    if (gapi) {
-      gapi.load('client:auth2', () => {
-        gapi.client.init({
+    if (gapi.gapi) {
+      gapi.gapi.load('client:auth2', () => {
+        gapi.gapi.client.init({
           clientId: '84382277177-tk0ct3n22t6pcshpjjadnbohq97rv2hv.apps.googleusercontent.com',
           scope: 'email'
         })
