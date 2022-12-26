80293af7f955ab8de75bbe091322515a0424be1f
ngocdiem138 <19110335@student.hcmute.edu.vn>
2022-12-26 11:54:25 +0700

    no message

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -55,7 +55,7 @@ class Content extends React.Component {
             {
               this.props.isCandidate?
                 <TabPanel>
-                  <CandidateSettings />
+                  <EmployerSettings />
                 </TabPanel> : null
             }
           </TabPanels>
